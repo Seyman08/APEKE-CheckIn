51f009a9330564f7c3572aef6d33efe3ecc4820d
Seyman08 <oluwaseyioyekale8@gmail.com>
2025-07-11 14:57:44 -0700

    running tests soon- turn off comment for reverse gocode

--- checkin.js
+++ checkin.js
@@ -45,7 +45,7 @@ form.addEventListener('submit', async (e) => {
     }).addTo(map).bindPopup("🏢 Office Location");
 
     
-    if (distance > 0.00003) {
+    if (distance > 0.00002) {
       status.textContent = "❌ You are not within the allowed check-in area.";
       form.querySelector("button").disabled = false; // 🔓 Re-enable on invalid location
       return;
